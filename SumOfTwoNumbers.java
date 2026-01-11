881224e868c16bbf382f3ecf21635be81eff1734
Samiksha-mane <samikshamane2005@gmail.com>
2026-01-11 21:50:07 +0530

    Add files via upload

---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class SumOfTwoNumbers 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+        System.out.print("Enter 1st number :");
+         a = sc.nextInt();
+        System.out.print("Enter 2nd Number :");
+         b = sc.nextInt();
+        System.out.println("Sum of 2 nos = "+(a+b));
+    }
+}
