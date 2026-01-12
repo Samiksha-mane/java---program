ccc42d19c7ab357cb586f0793ccb7e905ab830dd
Samiksha-mane <samikshamane2005@gmail.com>
2026-01-12 18:09:00 +0530

    Add files via upload

---
+++ Check_EvenOdd.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Check_EvenOdd 
+{
+    public static void main(String[] args)
+    { 
+        System.out.print("Enter a number :");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+       
+        if( a % 2 == 0)
+        {
+            System.out.println("Even number :"+a);
+        }
+        else
+        {
+            System.out.println("Odd number :"+a);
+        }
+
+    }    
+}
